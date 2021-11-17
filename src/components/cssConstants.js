65d12fa2409639493fccadd597ecbc565465fed4
Johan August Østbye <Johan.August@outlook.com>
2021-11-17 14:54:46 +0100

    made the hex 6 lenght

--- src/components/cssConstants.js
+++ src/components/cssConstants.js
@@ -3,8 +3,8 @@ export const btn_primary =
   'linear-gradient(90deg, rgba(30,44,135,1) 0%, rgba(44,78,255,1) 27%, rgba(0,139,255,1) 100%)';
 export const darkbg = '#010606';
 export const lightbg = '#f9f9f9';
-export const white = '#fff';
-export const black = '#fff';
+export const white = '#ffffff';
+export const black = '#ffffff';
 
 export const lightText = '#f7f8fa';
 
